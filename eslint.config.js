aa8e01b958c13fb7f7b6a0708293d37bfdc02f32
yanagitani <takanori.yanagitani@gmail.com>
2025-04-21 07:50:59 +0900

    chore: linter(eslint)

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rules: {
+    "camelcase": ["error", { properties: "always" }],
+    "space-before-function-paren": ["error", "never"],
+    "space-before-blocks": "error",
+    "keyword-spacing": "error",
+    "object-curly-spacing": ["error", "always"],
+    "semi": ["error", "never"],
+  },
+  ignores: [
+    "./mid.js",
+  ],
+}
